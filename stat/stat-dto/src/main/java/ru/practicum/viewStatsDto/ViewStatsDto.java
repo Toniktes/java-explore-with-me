6cfde6215b81c4076bda686093f6cbb93bb88e6f
Anton Teslia <teslyaanton@yandex.ru>
2023-06-02 20:25:51 +0300

    fix: Service

--- stat/stat-dto/src/main/java/ru/practicum/viewStatsDto/ViewStatsDto.java
+++ stat/stat-dto/src/main/java/ru/practicum/viewStatsDto/ViewStatsDto.java
@@ -1,12 +1,17 @@
 package ru.practicum.viewStatsDto;
 
-import lombok.Builder;
-import lombok.Data;
+import lombok.*;
 
-@Data
-@Builder
+@Getter
+@Setter
 public class ViewStatsDto {
     private String app;
     private String uri;
-    private Integer hits;
+    private long hits;
+
+    public ViewStatsDto(String app, String uri, long hits) {
+        this.app = app;
+        this.uri = uri;
+        this.hits = hits;
+    }
 }
