24ee579a9b916e2873ea26aa228c8fe2a6db8ff1
Anton Teslia <teslyaanton@yandex.ru>
2023-06-06 11:48:16 +0300

    feat: add dto, add models, add enums

--- main-service/src/main/java/ru/practicum/main/dto/compilation/UpdateCompilationRequest.java
+++ main-service/src/main/java/ru/practicum/main/dto/compilation/UpdateCompilationRequest.java
@@ -3,16 +3,16 @@ package ru.practicum.main.dto.compilation;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import ru.practicum.main.dto.event.EventShortDto;
 
+import javax.validation.constraints.Size;
 import java.util.List;
 
-@Getter
 @Setter
+@Getter
 @NoArgsConstructor
-public class CompilationDto {
-    private Long id;
+public class UpdateCompilationRequest {
+    private List<Long> events;
     private Boolean pinned;
+    @Size(min = 1, max = 50)
     private String title;
-    private List<EventShortDto> events;
 }
